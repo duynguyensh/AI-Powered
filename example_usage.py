5b13d04ae623a03074164ccda547088429c40c2d
Violent <142183299+Threadlinee@users.noreply.github.com>
2025-07-13 14:49:05 +0200

    Update example_usage.py

--- example_usage.py
+++ example_usage.py
@@ -131,7 +131,6 @@ def main():
 
 
 if __name__ == "__main__":
-    # Set up logging
     logger.remove()
     logger.add(sys.stderr, level="INFO", format="<green>{time:HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>")
     
